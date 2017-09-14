ee5305703e78ae0b85957f45e321a4b4fbaf4384
nanigalli <nani16.galli@gmail.com>
2017-09-13 23:27:58 -0300

    Correccion de un archivo de swagger

--- flasgger/__init__.py
+++ flasgger/__init__.py
@@ -1,9 +1,3 @@
-
-__version__ = '0.6.6'
-__author__ = 'Bruno Rocha'
-__email__ = 'rochacbruno@gmail.com'
-
-
 from jsonschema import ValidationError  # noqa
 from .base import Swagger, Flasgger, NO_SANITIZER, BR_SANITIZER, MK_SANITIZER  # noqa
 from .utils import swag_from, validate, apispec_to_template  # noqa
