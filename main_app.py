53d974e3b778973bfdce54c42978185389864691
Unknown <niko.sibi@gmail.com>
2017-09-13 21:37:51 -0300

    SUbo cambios para que heroku pueda soportar el loggeo, ya que no contempla el guardado de archivos de log.

--- main_app.py
+++ main_app.py
@@ -1,10 +1,9 @@
 """ @package main
 """
-import logging
+import os
 from flask import Flask, jsonify, abort, make_response, request
 from api.client_controller import ClientController
 from api.client_controller import TIPO_CLIENTE
-from logging.handlers import RotatingFileHandler
 
 application = Flask(__name__)
 CLIENT_CONTROLLER = ClientController()
@@ -53,6 +52,7 @@ def logTest():
 @application.route('/api/v1/clientedefault', methods=['GET'])
 def client_default():
     """Devuelve un ejemplo de la informacion que se debe enviar de un cliente"""
+    application.logger.info('[GET] /api/v1/clientedefault')
     response = CLIENT_CONTROLLER.get_info_new_client(TIPO_CLIENTE)
     response.status_code = 200
     return response
@@ -61,18 +61,21 @@ def client_default():
 def get_info_client(client_id):
     """Devuelve la informacion de un cliente
     @param client_id es el identificador del cliente"""
+    application.logger.info('[GET] /api/v1/client/' + str(client_id))
     response = CLIENT_CONTROLLER.get_client(client_id)
     return response
 
 @application.route('/api/v1/clients', methods=['GET'])
 def get_info_clients():
     """Devuelve la informacion de todos los clientes"""
+    application.logger.info('[GET] /api/v1/clients')
     response = CLIENT_CONTROLLER.get_clients(TIPO_CLIENTE)
     return response
 
 @application.route('/api/v1/client', methods=['POST'])
 def post_info_client():
     """Crea un nuevo cliente"""
+    application.logger.info('[POST] /api/v1/client')
     if not request.json:
         abort(400)
     response = CLIENT_CONTROLLER.post_new_client(request.json, TIPO_CLIENTE)
@@ -82,6 +85,7 @@ def post_info_client():
 def put_info_client(client_id):
     """Modificar un cliente
     @param client_id es el identificador del cliente"""
+    application.logger.info('[PUT] /api/v1/client/' + str(client_id))
     if not request.json:
         abort(400)
     response = CLIENT_CONTROLLER.put_new_client(request.json, TIPO_CLIENTE, client_id)
@@ -91,6 +95,7 @@ def put_info_client(client_id):
 def delete_info_client(client_id):
     """Devuelve la informacion de un cliente
     @param client_id es el identificador del cliente"""
+    application.logger.info('[DELETE] /api/v1/client/' + str(client_id))
     response = CLIENT_CONTROLLER.delete_client(client_id)
     return response
 
@@ -99,39 +104,27 @@ def delete_info_client(client_id):
 @application.route('/')
 def hello_word():
     """Devuelve el famoso Hello world"""
+    application.logger.info('[TEST] Hello world module - Hello World!')
     return jsonify(message='hello world')
 
 if __name__ == '__main__':
 
-    if not app.debug and os.environ.get('HEROKU') is None:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('microblog startup')
+    if not application.debug and os.environ.get('HEROKU') is None:
+        import logging
+        from logging.handlers import RotatingFileHandler
+        file_handler = RotatingFileHandler('access.log', 'a', 1 * 1024 * 1024, 10)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        application.logger.addHandler(file_handler)
+        application.logger.setLevel(logging.INFO)
+        application.logger.info('Logger startup - NO HEROKU')
 
-if os.environ.get('HEROKU') is not None:
-    import logging
-    stream_handler = logging.StreamHandler()
-    app.logger.addHandler(stream_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('microblog startup')
-
-    #Inicializo el log handler
-    logHandler = RotatingFileHandler('access.log', maxBytes=1000, backupCount=1)
-
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    logHandler.setFormatter(formatter)
-
-    #Seteo el nivel de logueo en INFO
-    logHandler.setLevel(logging.INFO)
-
-    #seteo el nivel de logueo de la APP en INFO
-    application.logger.setLevel(logging.INFO)
-
-    application.logger.addHandler(logHandler)
+    if os.environ.get('HEROKU') is not None:
+        import logging
+        stream_handler = logging.StreamHandler()
+        application.logger.addHandler(stream_handler)
+        application.logger.setLevel(logging.INFO)
+        application.logger.info('Logger startup')
+        application.logger.handlers.extend(logging.getLogger("gunicorn.error").handlers)
 
     application.run(host='0.0.0.0')
