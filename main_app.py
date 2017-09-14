8f98893a79fdcd9a2888ab7afbc79a7b99b65b4d
Unknown <niko.sibi@gmail.com>
2017-09-13 21:02:48 -0300

    Subo el logger mergeando con dev, no hago merge desde el branh debido al rebase que hicimos.

--- main_app.py
+++ main_app.py
@@ -1,8 +1,10 @@
 """ @package main
 """
+import logging
 from flask import Flask, jsonify, abort, make_response, request
 from api.client_controller import ClientController
 from api.client_controller import TIPO_CLIENTE
+from logging.handlers import RotatingFileHandler
 
 application = Flask(__name__)
 CLIENT_CONTROLLER = ClientController()
@@ -22,11 +24,11 @@ tasks = [
     }
 ]
 
-@application.route('/todo/api/v1.0/tasks', methods=['GET'])
+@application.route('/todo/api/v1/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-@application.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@application.route('/todo/api/v1/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
@@ -35,7 +37,16 @@ def get_task(task_id):
 
 @application.errorhandler(404)
 def not_found(error):
+    """Manejador de error para codigo 404"""
+    application.logger.error('Error 404 - Recurso no encontrado')
     return make_response(jsonify({'error': 'Not Found'}), 404)
+
+@application.route('/logtest')
+def logTest():
+    application.logger.warning('Testeando Warning!')
+    application.logger.error('Testeando Error!')
+    application.logger.info('Testeando Info!')
+    return "Testeando el Logger..."
 
 #Endpoints de clientes
 
@@ -83,10 +94,44 @@ def delete_info_client(client_id):
     response = CLIENT_CONTROLLER.delete_client(client_id)
     return response
 
+
+
 @application.route('/')
 def hello_word():
     """Devuelve el famoso Hello world"""
     return jsonify(message='hello world')
 
 if __name__ == '__main__':
+
+    if not app.debug and os.environ.get('HEROKU') is None:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+    #Inicializo el log handler
+    logHandler = RotatingFileHandler('access.log', maxBytes=1000, backupCount=1)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logHandler.setFormatter(formatter)
+
+    #Seteo el nivel de logueo en INFO
+    logHandler.setLevel(logging.INFO)
+
+    #seteo el nivel de logueo de la APP en INFO
+    application.logger.setLevel(logging.INFO)
+
+    application.logger.addHandler(logHandler)
+
     application.run(host='0.0.0.0')
