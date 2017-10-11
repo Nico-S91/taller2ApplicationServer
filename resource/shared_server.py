4ad7548ccf73af19157e497ed693c60a7433ff44
nanigalli <nani16.galli@gmail.com>
2017-10-10 23:32:49 -0300

    Hice un refactor para las peticiones del shared server

--- resource/shared_server.py
+++ resource/shared_server.py
@@ -17,79 +17,42 @@ class SharedServer:
         @param username es el nombre del usuario que guardo en el sistema
         @param password es la contraseña del usuario"""
         credential = client_shared.get_json_client_credentials(username, password)
-        self._refresh_token()
-        url = self._get_url('/api/v1/users/validate')
-        response_server = requests.post(url, json=credential)
-        json_data = json.loads(response_server.text)
-
-        if response_server.status_code == 200:
-            response = jsonify(json_data)
-            response.status_code = 200
-        else:
-            response = jsonify(code=1, message='Ese cliente no existe')
-            response.status_code = 401
-        return response
+        return self._get_validate_client(credential)
 
     def get_validate_client_facebook(self, facebook_auth_token):
         """Validamos que el usuario exista en el sistema
         @param facebookAuthToken es el token de facebook que tenemos guardado en el sistema"""
         credential = client_shared.get_json_client_credentials_facebook(facebook_auth_token)
-        self._refresh_token()
-        url = self._get_url('/api/v1/users/validate')
-        response_server = requests.post(url, json=credential)
-        json_data = json.loads(response_server.text)
-
-        if response_server.status_code == 200:
-            response = jsonify(json_data)
-            response.status_code = 200
-        else:
-            response = jsonify(code=1, message='Ese cliente no existe')
-            response.status_code = 401
-        return response
+        return self._get_validate_client(credential)
 
     def put_client(self, client_id, client):
         """ Modifica la informacion de un cliente/chofer
             @param client es la informacion modificada del cliente/chofer existente
         """
-        self._refresh_token()
-        url = self._get_url('/api/v1/users/'+str(client_id))
-        response_server = requests.put(url, json=client)
-        return response_server
+        return self._put_shared_server('/api/v1/users/'+str(client_id), client)
 
     def post_client(self, client):
         """ Crea un nuevo cliente/chofer
             @param client es la informacion del cliente/chofer
         """
-        self._refresh_token()
-        url = self._get_url('/api/v1/users')
-        response_server = requests.post(url, json=client)
-        return response_server
+        return self._post_shared_server('/api/v1/users', client)
 
     def get_client(self, client_id):
         """ Devuelve la informacion del cliente buscado
             @param client_id es el id del cliente buscado
         """
-        self._refresh_token()
-        url = self._get_url('/api/v1/users/'+str(client_id))
-        response_server = requests.get(url)
-        return response_server
+        return self._get_shared_server('/api/v1/users/'+str(client_id))
 
     def get_clients(self):
         """ Devuelve la informacion de los clientes/choferes
         """
-        self._refresh_token()
-        url = self._get_url('/api/v1/users')
-        response_server = requests.get(url)
-        return response_server
+        return self._get_shared_server('/api/v1/users')
 
     def delete_client(self, client_id):
         """ Elimina un cliente/chofer
             @param client_id es el id del cliente/chofer que se desea eliminar
         """
-        self._refresh_token()
-        url = self._get_url('/api/v1/users/'+str(client_id))
-        response_server = requests.delete(url)
-        return response_server
+        return self._delete_shared_server('/api/v1/users/'+str(client_id))
 
     # Metodos para manipular autos
 
@@ -98,32 +61,7 @@ class SharedServer:
             @param id_car es el id del auto del cliente
             @param owner es el id del cliente buscado
         """
-        #Aca va a ir el codigo para hacer el pedido de get del auto del cliente
-        metadata = {
-                "version": "1"
-            }
-        car = {
-            "id": "1",
-            "_ref": "dfsd",
-            "owner": "23",
-            "properties": [
-            {
-                "name": "color",
-                "value": "negro"
-            },
-            {
-                "name": "modelo",
-                "value": "punto"
-            },
-            {
-                "name": "marca",
-                "value": "fiat"
-            }
-            ]
-        }
-        response = jsonify({'metadata': metadata, 'car': car})
-        response.status_code = 200
-        return response
+        return self._get_shared_server('/api/v1/users/'+str(owner)+'/cars/'+str(id_car))
 
     def post_car(self, properties, client_id):
         """ Crea un nuevo auto para el chofer
@@ -179,3 +117,51 @@ class SharedServer:
         """Devuelve la url formada para pegarle al shared server
         @param endpoint es el endpoint del shared server al que se le va a pegar"""
         return self.url_shared_server + endpoint + '?token=' + self.token
+
+    def _get_shared_server(self, endpoint):
+        """Get al endpoint
+        @param endpoint es el endpoint del shared server al que se le va a pegar"""
+        self._refresh_token()
+        url = self._get_url(endpoint)
+        response_server = requests.get(url)
+        return response_server
+
+    def _post_shared_server(self, endpoint, json_data):
+        """Post al endpoint
+        @param endpoint es el endpoint del shared server al que se le va a pegar
+        @param json_data es el json que se va a enviar en el post"""
+        self._refresh_token()
+        url = self._get_url(endpoint)
+        response_server = requests.post(url, json=json_data)
+        return response_server
+
+    def _put_shared_server(self, endpoint, json_data):
+        """Put al endpoint
+        @param endpoint es el endpoint del shared server al que se le va a pegar
+        @param json_data es el json que se va a enviar en el put"""
+        self._refresh_token()
+        url = self._get_url(endpoint)
+        response_server = requests.put(url, json=json_data)
+        return response_server
+
+    def _delete_shared_server(self, endpoint):
+        """Delete al endpoint
+        @param endpoint es el endpoint del shared server al que se le va a pegar"""
+        self._refresh_token()
+        url = self._get_url(endpoint)
+        response_server = requests.delete(url)
+        return response_server
+
+    def _get_validate_client(self, credential):
+        """Validamos que el usuario a partir de las credenciales
+        @param credential son las credenciales del usuario"""
+        response_server = self._post_shared_server('/api/v1/users/validate', credential)
+        json_data = json.loads(response_server.text)
+
+        if response_server.status_code == 200:
+            response = jsonify(json_data)
+            response.status_code = 200
+        else:
+            response = jsonify(code=1, message='Ese cliente no existe')
+            response.status_code = 401
+        return response
