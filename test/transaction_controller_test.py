2f9306e2255010b25167a7542e60235bb156df4d
nanigalli <nani16.galli@gmail.com>
2017-10-21 15:39:04 -0300

    Agrego los tests para las transacciones de los clientes

--- test/transaction_controller_test.py
+++ test/transaction_controller_test.py
@@ -94,3 +94,102 @@ class TestTransactionController(unittest.TestCase):
         self.assertEqual(response.status_code, 401)
         cmp_response = json.loads(response.data.decode('utf-8'))
         self.assertEqual(assert_res, cmp_response)
+
+    #GET transacciones de clientes
+
+    def test_informacion_transacciones_cliente(self):
+        """Prueba que al obtener las transacciones de un cliente"""
+        #Mockeamos la llamada
+        self.mockeamos_login_correcto()
+        response_mock = ResponseMock()
+        response_shared = json.dumps({
+            'metadata': {
+                'count': 2,
+                'total': 2,
+                'next': 'string',
+                'prev': 'string',
+                'first': 'string',
+                'last': 'string',
+                'version': 'xx'
+            },
+            'transactions': [
+                {
+                'id': '1',
+                'trip': '1',
+                'timestamp': 0,
+                'cost': {
+                    'currency': 'pesos',
+                    'value': 100
+                },
+                'description': 'Es que me hizo esperar mucho',
+                'data': {}
+                },
+                {
+                'id': '1',
+                'trip': '1',
+                'timestamp': 0,
+                'cost': {
+                    'currency': 'pesos',
+                    'value': 48
+                },
+                'description': 'Nada para acotar',
+                'data': {}
+                }
+            ]
+        })
+        response_mock.set_response(response_shared)
+        response_mock.set_code(200)
+        SharedServer.get_transactions = MagicMock(return_value=response_mock)
+        #Hacemos la llamada normal
+        response = self.app.get('/api/v1/client/23/transactions')
+        assert_res = json.loads("""
+        [
+            {
+            "id": "1",
+            "trip": "1",
+            "timestamp": 0,
+            "cost": {
+                "currency": "pesos",
+                "value": 100
+            },
+            "description": "Es que me hizo esperar mucho",
+            "data": {}
+            },
+            {
+            "id": "1",
+            "trip": "1",
+            "timestamp": 0,
+            "cost": {
+                "currency": "pesos",
+                "value": 48
+            },
+            "description": "Nada para acotar",
+            "data": {}
+            }
+        ]""")
+        self.assertEqual(response.status_code, 200)
+        cmp_response = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(assert_res, cmp_response)
+
+    def test_obtener_transacciones_cliente_sin_autorizacion(self):
+        """Prueba que al obtener la informacion de las transacciones de un cliente sin autorizacion"""
+        #Mockeamos la llamada
+        self.mockeamos_login_correcto()
+        response_mock = ResponseMock()
+        response_shared = json.dumps({
+            'code': '0',
+            'message': 'Ups...no tiene autorizacion'
+        })
+        response_mock.set_response(response_shared)
+        response_mock.set_code(401)
+        SharedServer.get_transactions = MagicMock(return_value=response_mock)
+        #Hacemos la llamada normal
+        response = self.app.get('/api/v1/client/8/transactions')
+        assert_res = json.loads("""
+        {
+            "code": "0",
+            "message": "Ups...no tiene autorizacion"
+        }""")
+        self.assertEqual(response.status_code, 401)
+        cmp_response = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(assert_res, cmp_response)
