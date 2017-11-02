0c6aea4a23425282fde7ee3a1a30495f0cae13c5
Nico <niko.sibi@gmail.com>
2017-11-01 22:39:49 -0300

    Agrego un pequeño fix

--- api/model_manager.py
+++ api/model_manager.py
@@ -113,6 +113,28 @@ class ModelManager:
         
         return result
 
+    def get_trip(self, trip_id):
+        """ Este metodo obtiene el viaje dado su id
+            @param trip_id el id del viaje
+        """
+
+        viajes = self.db_manager.get_table('viajes')
+        viaje = viajes.find_one({'idViaje': trip_id})
+
+        if viaje is not None:
+            response = {
+                "trip_id": viaje.get('idViaje'),
+                "driver_id": viaje.get('idDriver'),
+                "passenger_id": viaje.get('idPassenger'),
+                "trip": viaje.get('trip'),
+                "paymethod": viaje.get('paymethod'),
+                "route": viaje.get('route'),
+                "aceptoViaje": viaje.get('aceptoViaje')
+            }
+            return response
+
+        return None
+
     def add_last_known_position(self, user_id, user_type, latitud, longitud):
         """ Este metodo graba en la base de datos 'UltimasPosiciones'
             la ultima posicion registrada del usuario.
@@ -148,28 +170,6 @@ class ModelManager:
                     'long': longitud
                 }
             }, upsert=False).acknowledged
-
-    def get_trip(self, trip_id):
-        """ Este metodo obtiene el viaje dado su id
-            @param trip_id el id del viaje
-        """
-
-        viajes = self.db_manager.get_table('viajes')
-        viaje = viajes.find_one({'idViaje': trip_id})
-
-        if viaje is not None:
-            response = {
-                "trip_id": viaje.get('idViaje'),
-                "driver_id": viaje.get('idDriver'),
-                "passenger_id": viaje.get('idPassenger'),
-                "trip": viaje.get('trip'),
-                "paymethod": viaje.get('paymethod'),
-                "route": viaje.get('route'),
-                "aceptoViaje": viaje.get('aceptoViaje')
-            }
-            return response
-
-        return None
 
     def get_last_known_position(self, client_id):
         """Este metodo obtiene la ultima posicion conocida por
