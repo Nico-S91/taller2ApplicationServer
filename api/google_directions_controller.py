7e001eafb56dd365d05471a399955fc22b582540
Nico <niko.sibi@gmail.com>
2017-11-19 12:50:20 -0300

    subo los primeros metodos para la obtencion de direcciones de la API de google

--- api/google_directions_controller.py
+++ api/google_directions_controller.py
@@ -1,7 +1,8 @@
 """ @package google_directions_controller
 """
+import requests
 
-GOOGLE_API_KEY = ""
+GOOGLE_API_KEY = "AIzaSyC0Ro5dttq9RUlR78Lta4_exF8fIK1qgxM"
 GOOGLE_URL = "http://maps.googleapis.com/maps/api/directions/json?"
 
 class DirectionsController:
@@ -12,10 +13,16 @@ class DirectionsController:
     def __init__(self):
         """Constructor"""
 
-    def get_possible_routes(self, route_request):
-        """ Este metodo devuelve las posibles rutas que encontro la API de google
-            entre dos pares [latitud, longitud]
-            @param route_request los dos puntos del mapa que queremos encontrarle rutas
+    def _get_google_directions(self, parameters):
+        """ Get al endpoint de google directions api
+            @param parameters los parametros de busqueda para la api
         """
+        url = self._get_url(parameters)
+        response_google_api = requests.get(url)
+        return response_google_api
 
-
+    def _get_url(self, parameters):
+        """ Devuelve la url formada para pegarle a la api de google
+            @param clase con parametros para pegarle a la api
+        """
+        return GOOGLE_URL + "origin=" + str(parameters["origin_lat"]) + "," + str(parameters["origin_long"]) + "&destination=" + str(parameters["destination_lat"]) + "," + str(parameters["destination_long"]) + "&key=" + GOOGLE_API_KEY
