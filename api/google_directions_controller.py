fc6ff5b53b2db4a18042087d56f711eb3b6a0935
Nico <niko.sibi@gmail.com>
2017-11-19 14:29:26 -0300

    endpoint de peticion de rutas a la api de google funcionando!

--- api/google_directions_controller.py
+++ api/google_directions_controller.py
@@ -1,9 +1,11 @@
 """ @package google_directions_controller
 """
 import requests
+import json
+from flask import jsonify
 
 GOOGLE_API_KEY = "AIzaSyC0Ro5dttq9RUlR78Lta4_exF8fIK1qgxM"
-GOOGLE_URL = "http://maps.googleapis.com/maps/api/directions/json?"
+GOOGLE_URL = "https://maps.googleapis.com/maps/api/directions/json?"
 
 class DirectionsController:
     """ Esta clase contiene los metodos de comunicacion con la
@@ -13,13 +15,14 @@ class DirectionsController:
     def __init__(self):
         """Constructor"""
 
-    def _get_google_directions(self, parameters):
+    def get_google_directions(self, parameters):
         """ Get al endpoint de google directions api
             @param parameters los parametros de busqueda para la api
         """
         url = self._get_url(parameters)
         response_google_api = requests.get(url)
-        return response_google_api
+        json_data = json.loads(response_google_api.text)
+        return jsonify(json_data)
 
     def _get_url(self, parameters):
         """ Devuelve la url formada para pegarle a la api de google
